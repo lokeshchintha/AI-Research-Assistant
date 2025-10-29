1e5aa1db1b78bc55948a8d2299efc2599a00364a
lokeshchintha <20a91a05e6@aec.edu.in>
2025-10-29 09:05:22 +0530

    First Commit

--- backend/server.js
+++ backend/server.js
@@ -136,7 +136,7 @@ io.on('connection', (socket) => {
 
 const PORT = process.env.PORT || 5000;
 
-httpServer.listen(PORT, () => {
+httpServer.listen(PORT,'0.0.0.0', () => {
   console.log(`🚀 Server running on port ${PORT}`);
   console.log(`📡 Socket.io ready for real-time collaboration`);
 });
